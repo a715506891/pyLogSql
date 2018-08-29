a7f2c9ddd4694fd44aeaf95fb00b5a3bd8f75c87
niuchen <a715506891@163.com>
2018-08-29 10:25:57 +0800

    Update 数据分组.sql

--- 数据分组.sql
+++ 数据分组.sql
@@ -1,7 +1,7 @@
 SELECT COUNT(CASE WHEN IFNULL(total_money, 0) >=1000 THEN a.customer_id END ) AS '大于1000'
 		,COUNT(CASE WHEN IFNULL(total_money, 0) >=800 AND IFNULL(total_money, 0)<800 THEN a.customer_id END ) AS '800~1000'
 		,COUNT(CASE WHEN IFNULL(total_money, 0) <800 THEN a.customer_id END) AS '小于800'
-	FROM customer_login AS a 
+	FROM customer AS a 
 	LEFT JOIN
-		(SELECT customer_id, COUNT(order_money) AS total_money FROM order_master GROUP BY customer_id) AS b
-	ON a.customer_id=b.customer_id;
+		(SELECT cust_id, COUNT(oder_money) AS toal_money FROM ordr_mster GROUP BY cust_id) AS b
+	ON a.cust_id=b.cust_id;
